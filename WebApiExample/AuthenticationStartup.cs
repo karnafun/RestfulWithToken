e879e1105f830a773b1b9417aa0bc182012b060e
dordan <dordan@matrix.co.il>
2020-12-31 14:35:04 +0200

    C:/Program Files/Git/api/data/forall endpoint is working. Still not getting a token from /token

---
+++ WebApiExample/AuthenticationStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+using WebApiExample.App_Start;
+
+namespace WebApiExample
+{
+	public class AuthenticationStartup
+	{
+		public void Configuration(IAppBuilder app) {
+
+			app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+			var myProvider = new ServerAuthenticationProvider();
+			OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+			{
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString("/token"),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+				Provider = myProvider
+			};
+
+			app.UseOAuthAuthorizationServer(options);
+			app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+			HttpConfiguration config = new HttpConfiguration();
+			WebApiConfig.Register(config);
+		}
+	}
+}
---
+++ WebApiExample/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+
+namespace WebApiExample.Controllers
+{
+    public class UserController : ApiController
+    {
+
+
+
+
+
+		[AllowAnonymous]
+		[HttpGet]
+		[Route("api/data/forall")]
+		public IHttpActionResult Get()
+		{
+
+			return Ok("now server time is " + DateTime.Now.ToString());
+		}
+
+		[Authorize]
+        [HttpGet]
+        [Route("api/data/authenticate")]
+        public IHttpActionResult GetForAuthenticate() { 
+        
+            var identity = (ClaimsIdentity)User.Identity;
+            return Ok("Hello " + identity.Name);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [Route("api/data/authorize")]
+        public IHttpActionResult GetForAdmin()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var roles = identity.Claims
+                        .Where(c => c.Type == ClaimTypes.Role)
+                        .Select(c => c.Value);
+            return Ok("Hello " + identity.Name + " Role: " + string.Join(",", roles.ToList()));
+        }
+    }
+}

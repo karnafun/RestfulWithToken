8bddc465146a6d384f96733c8cfe07c378c41837
dordan <dordan@matrix.co.il>
2021-01-05 09:20:36 +0200

    added ajaxCalls.js to split logic a bit

---
+++ WebApiExample/Client/AjaxCalls.js
@@ -0,0 +1,31 @@
+﻿
+let  
+
+function AuthenticateFunction(username, password, successCB, errorCB) {
+    let access_token = localStorage.getItem("access_token");     
+
+    if (access_token == undefined || access_token == null || access_token.length < 10) {
+
+        errCB({responseText: 'No access_token'})
+        return;
+    }
+    let data = {
+        username: username,
+        password: password,
+        grant_type: "password"
+    }
+
+    let ajaxCallObject = {
+        url: "https://localhost:44326/api/data/authenticate",
+        type: "GET",
+        data: data,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+        },
+        success: successCB,
+        error: errorCB,
+
+    }
+    $.ajax(ajaxCallObject);
+
+}
